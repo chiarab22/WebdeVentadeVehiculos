d7a3b92738131f1cd8073ee154cda1f3ea6fca6e
Patriesc <98825807+Patriesc@users.noreply.github.com>
2022-02-28 17:03:46 +0100

    Add spring boot initializr

---
+++ VentaDeVehiculos/VentaDeVehiculos/src/test/java/com/ventaDeVehiculos/VentaDeVehiculos/VentaDeVehiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ventaDeVehiculos.VentaDeVehiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentaDeVehiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
