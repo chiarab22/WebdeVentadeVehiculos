22a53a5c69bc8aef11a9c0515b507793197a75f4
chiarab22 <cbocc@myuax.com>
2022-03-22 19:00:15 +0100

    mejoras

--- patrones-construccion/src/App.java
+++ patrones-construccion/src/App.java
@@ -6,8 +6,10 @@ public class App {
     public static void main(String[] args) {
         Catalogo catalogo = new Catalogo();
         catalogo.catalogos();
+        System.out.println();
         Usuario usuario = new Usuario();
         usuario.crearUsuario();
+        System.out.println();
         TestComercial testComercial = new TestComercial();
         testComercial.comercial();
 
