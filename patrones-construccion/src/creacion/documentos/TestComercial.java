0085fac3fd758b9550fce5db144548218f4b2b8d
Patriesc <patricia.sanz.escurin@gmail.com>
2022-04-17 19:47:14 +0200

    corrección imports

--- patrones-construccion/src/creacion/documentos/TestComercial.java
+++ patrones-construccion/src/creacion/documentos/TestComercial.java
@@ -1,19 +1,18 @@
-package documentos;
-public class TestComercial  
-{  
+package creacion.documentos;
 
-   public void comercial(){
-    // inicialización del comercial en el sistema  
-    Comercial elComercial = Comercial.Instance();  
-    elComercial.setNombre("Comercial Auto");  
-    elComercial.setDireccion("Madrid");  
+public class TestComercial {
+
+  public void comercial() {
+    // inicialización del comercial en el sistema
+    Comercial elComercial = Comercial.Instance();
+    elComercial.setNombre("Comercial Auto");
+    elComercial.setDireccion("Madrid");
     elComercial.setEmail("comercial@comerciales.com");
-    // muestra el comercial del sistema  
-    visualiza();  
-  }  
-  
-  public static void visualiza()
-  {
+    // muestra el comercial del sistema
+    visualiza();
+  }
+
+  public static void visualiza() {
     Comercial elComercial = Comercial.Instance();
     elComercial.visualiza();
   }
